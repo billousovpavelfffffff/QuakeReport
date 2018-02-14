0e4b013b450c82a40c1e4c8e3d654c51364aeef4
billousovpavelfffffff <pavlo.bilousov@nure.ua>
2018-02-14 18:31:03 +0200

    Fix, all work!!

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -28,20 +29,53 @@ import java.util.ArrayList;
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=1&limit=20";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
+        EathquakeTast task = new EathquakeTast();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+
+
+
+
+
+    private class EathquakeTast extends AsyncTask<String, Void,  ArrayList<QuakeInf>> {
+        @Override
+        protected  ArrayList<QuakeInf> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            ArrayList<QuakeInf> result = QueryUtils.fetchEarthquakeData(USGS_REQUEST_URL);
+            return result;
+        }
+        protected void onPostExecute( ArrayList<QuakeInf> resault) {
+            if (resault == null) {
+                return;
+            }
+            updateUi(resault);
+        }
+    }
+
+
+
+
+    private void updateUi (ArrayList<QuakeInf> earthquakes) {
+
         // Массив созданных землятресений
-        ArrayList<QuakeInf> earthquakes = QueryUtils.extractEarthquakes();
+        ArrayList<QuakeInf> earthquake = earthquakes;
 
         // Найти ListView
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Создания нового ArrayAdapter
-        final QuakeInfAdapter quakeInfAdapter = new QuakeInfAdapter(this, earthquakes);
+        final QuakeInfAdapter quakeInfAdapter = new QuakeInfAdapter(this, earthquake);
         earthquakeListView.setAdapter(quakeInfAdapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
